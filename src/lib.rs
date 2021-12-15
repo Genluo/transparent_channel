58e20ffc193a842f9087fefc7d697ef77892dece
云墨 <lg208143@alibaba-inc.com>
2021-12-15 16:01:30 +0800

    feat: add batch function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use bytes::Bytes;
 use image::{io::Reader, DynamicImage, ImageBuffer, Rgba};
-use image::{GenericImage, ImageError, Pixel};
+use image::{ImageError, Pixel};
 use reqwest::{get, Error};
 use std::{io::Cursor, panic};
 
@@ -18,8 +18,23 @@ pub async fn get_image_list(uri_list: &Vec<String>) -> Result<Vec<Bytes>, Error>
     Ok(result)
 }
 
-pub async fn transparent_channel() -> Result<(), ImageError> {
-    let img_content = match get_image("https://gw.alicdn.com/imgextra/i3/O1CN01tqFLVn1oddFYIRjUr_!!6000000005248-2-tps-1200-1200.png").await {
+pub async fn batch(list: &Vec<String>) -> Result<Vec<ImageBuffer<Rgba<u8>, Vec<u8>>>, ImageError>{
+    let mut result =vec![];
+    let suffix = ".png".to_string();
+    let mut index = 1;
+
+    for url in list {
+        let img = transparent_channel(url).await?;
+        img.save(&(index.to_string() + &suffix));
+        index += 1;
+        result.push(img);
+    }
+
+    Ok(result)
+}
+
+pub async fn transparent_channel(uri: &str) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>, ImageError> {
+    let img_content = match get_image(uri).await {
         Ok(content) => {
             content
         },
@@ -35,34 +50,33 @@ pub async fn transparent_channel() -> Result<(), ImageError> {
         Err(err) => panic!("生成image buff失败 {:?}", err),
     };
 
-    let (gray_image, white_out) = match create_gray_img(&d_image) {
+    let out = match create_gray_img(&d_image) {
         Some(a) => a,
         None => panic!("灰度生成失败！"),
     };
 
-    Ok(())
+    Ok(out)
 }
 
-pub fn create_gray_img(_img: &DynamicImage) -> Option<(ImageBuffer<Rgba<u8>, Vec<u8>>, ImageBuffer<Rgba<u8>, Vec<u8>>)> {
+pub fn create_gray_img(_img: &DynamicImage) -> Option<ImageBuffer<Rgba<u8>, Vec<u8>>> {
     if let DynamicImage::ImageRgba8(image) = _img {
         let (width, height) = image.dimensions();
-        let mut grey_out = ImageBuffer::new(width, height);
-        let mut white_out = ImageBuffer::new(width, height);
+        let mut out = ImageBuffer::new(width * 2, height);
 
         for y in 0..height {
             for x in 0..width {
                 let p = image.get_pixel(x, y);
                 let current_alpha_channel = p.channels()[3];
                 if current_alpha_channel == 255 {
-                    grey_out.put_pixel(x, y, Pixel::from_channels(0, 0, 0, 255));
-                    white_out.put_pixel(x, y, *p);
+                    out.put_pixel(x + width, y, Pixel::from_channels(0, 0, 0, 255));
+                    out.put_pixel(x, y, *p);
                 } else {
-                    white_out.put_pixel(x, y, Pixel::from_channels(255, 255, 255, 255));
-                    grey_out.put_pixel(x, y, Pixel::from_channels(255, 255, 255, 255));
+                    out.put_pixel(x + width, y, Pixel::from_channels(255, 255, 255, 255));
+                    out.put_pixel(x, y, Pixel::from_channels(255, 255, 255, 255));
                 }
             }
         }
-        return Some((grey_out, white_out));
+        return Some(out);
     }
     None
 }
@@ -95,8 +109,20 @@ mod test {
 
     #[actix_rt::test]
     async fn test_translate_channel() -> Result<(), ImageError> {
-        transparent_channel().await?;
+        transparent_channel("https://gw.alicdn.com/imgextra/i3/O1CN01tqFLVn1oddFYIRjUr_!!6000000005248-2-tps-1200-1200.png").await?;
         assert_eq!(1, 1);
+        Ok(())
+    }
+
+    #[actix_rt::test]
+    async fn test_batch() -> Result<(), ImageError> {
+        let image_list = vec![
+            String::from("https://gw.alicdn.com/imgextra/i3/O1CN01tqFLVn1oddFYIRjUr_!!6000000005248-2-tps-1200-1200.png"),
+            String::from("https://gw.alicdn.com/imgextra/i4/O1CN01xsBzZy1wPuJ9GW5fC_!!6000000006301-2-tps-1200-1200.png")
+        ];
+
+        let content = batch(&image_list).await?;
+        assert_eq!(image_list.len(), content.len());
         Ok(())
     }
 }
